8f1b4b03a0e0763145596e5ee16600c0e9ec55c4
alterphp <pc.bertineau@alterphp.com>
2017-05-10 23:04:57 +0200

    override EasyAdmin templates namespace

--- EasyAdminExtensionBundle.php
+++ EasyAdminExtensionBundle.php
@@ -2,8 +2,19 @@
 
 namespace AlterPHP\EasyAdminExtensionBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EasyAdminExtensionBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        // Prepend EasyAdminExtension bundle tmeplates to EasyAdmin namespace
+        $container->prependExtensionConfig(
+            'twig',
+            ['paths' => [__DIR__.'/Resources/views' => 'EasyAdmin']]
+        );
+    }
 }
