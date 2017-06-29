7f20334442ba8c77da1ec7b9aa7e26cd2fa396cc
alterphp <pc.bertineau@alterphp.com>
2017-06-30 01:02:41 +0200

    Allow defining its custom types with short names

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,6 +24,15 @@ class Configuration implements ConfigurationInterface
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
 
+        $rootNode
+            ->children()
+                ->arrayNode('custom_types')
+                    ->useAttributeAsKey('short_name')
+                    ->prototype('scalar')->end()
+                ->end()
+            ->end()
+        ;
+
         return $treeBuilder;
     }
 }
--- DependencyInjection/EasyAdminExtensionExtension.php
+++ DependencyInjection/EasyAdminExtensionExtension.php
@@ -22,6 +22,8 @@ class EasyAdminExtensionExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->setParameter('easy_admin_extension.custom_types', $config['custom_types']);
+
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
