e23d4a40a1fb38bf086ef5477ad7f80a022d9052
PéCé <pc.bertineau@alterphp.com>
2017-09-25 00:34:28 +0200

    Fix tests

--- EventListener/PostQueryBuilderSubscriber.php
+++ EventListener/PostQueryBuilderSubscriber.php
@@ -3,7 +3,7 @@
 namespace AlterPHP\EasyAdminExtensionBundle\EventListener;
 
 use Doctrine\ORM\QueryBuilder;
-use JavierEguiluz\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
+use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
