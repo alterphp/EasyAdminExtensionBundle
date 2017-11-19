ba317cac5a5528d6779815e0dfc9b55923950130
PéCé <pc.bertineau@alterphp.com>
2017-11-19 12:16:56 +0100

    refactor PostQueryBuilderListener for future evolutions

--- src/EventListener/PostQueryBuilderSubscriber.php
+++ src/EventListener/PostQueryBuilderSubscriber.php
@@ -7,8 +7,14 @@ use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
+/**
+ * Apply filters on list/search queryBuilder
+ */
 class PostQueryBuilderSubscriber implements EventSubscriberInterface
 {
+    /**
+     * {@inheritdoc}
+     */
     public static function getSubscribedEvents()
     {
         return array(
@@ -17,6 +23,11 @@ class PostQueryBuilderSubscriber implements EventSubscriberInterface
         );
     }
 
+    /**
+     * Called on POST_LIST_QUERY_BUILDER event.
+     *
+     * @param  GenericEvent $event
+     */
     public function onPostListQueryBuilder(GenericEvent $event)
     {
         $queryBuilder = $event->getArgument('query_builder');
@@ -26,6 +37,11 @@ class PostQueryBuilderSubscriber implements EventSubscriberInterface
         }
     }
 
+    /**
+     * Called on POST_SEARCH_QUERY_BUILDER event.
+     *
+     * @param  GenericEvent $event
+     */
     public function onPostSearchQueryBuilder(GenericEvent $event)
     {
         $queryBuilder = $event->getArgument('query_builder');
@@ -35,23 +51,71 @@ class PostQueryBuilderSubscriber implements EventSubscriberInterface
         }
     }
 
+    /**
+     * Applies filters on queryBuilder.
+     *
+     * @param  QueryBuilder $queryBuilder
+     * @param  array        $filters
+     */
     protected function applyRequestFilters(QueryBuilder $queryBuilder, array $filters = array())
     {
         foreach ($filters as $field => $value) {
-            // Add root entity alias if none provided
-            $fieldName = false === strpos($field, '.') ? $queryBuilder->getRootAlias().'.'.$field : $field;
-            // Sanitize parameter name
-            $parameterName = 'filter_'.str_replace('.', '_', $fieldName);
-            // For multiple value, use an IN clause, equality otherwise
-            if (is_array($value)) {
-                $filterDqlPart = $fieldName.' IN (:'.$parameterName.')';
-            } else {
-                $filterDqlPart = $fieldName.' = :'.$parameterName;
+            // Empty string in considered as "not applied filter" (it's supposed to be set from GET parameter)
+            if ('' === $value) {
+                continue;
             }
-            $queryBuilder
-                ->andWhere($filterDqlPart)
-                ->setParameter($parameterName, $value)
-            ;
+
+            // Add root entity alias if none provided
+            $field = false === strpos($field, '.') ? $queryBuilder->getRootAlias().'.'.$field : $field;
+
+            // Checks if filter is directly appliable on queryBuilder
+            if (!$this->isFilterAppliable($queryBuilder, $field)) {
+                continue;
+            }
+
+            // Sanitize parameter name
+            $parameter = 'filter_'.str_replace('.', '_', $field);
+
+            $this->filterQueryBuilder($queryBuilder, $field, $parameter, $value);
         }
+    }
+
+    /**
+     * Filters queryBuilder.
+     *
+     * @param  QueryBuilder $queryBuilder
+     * @param  string       $field
+     * @param  string       $parameter
+     * @param  mixed        $value
+     */
+    protected function filterQueryBuilder(QueryBuilder $queryBuilder, string $field, string $parameter, $value)
+    {
+        // For multiple value, use an IN clause, equality otherwise
+        if (is_array($value)) {
+            $filterDqlPart = $field.' IN (:'.$parameter.')';
+        } else {
+            $filterDqlPart = $field.' = :'.$parameter;
+        }
+
+        $queryBuilder
+            ->andWhere($filterDqlPart)
+            ->setParameter($parameter, $value)
+        ;
+    }
+
+    /**
+     * Checks if filter is directly appliable on queryBuilder.
+     *
+     * @param  QueryBuilder $queryBuilder
+     * @param  string       $field
+     *
+     * @return boolean
+     */
+    protected function isFilterAppliable(QueryBuilder $queryBuilder, string $field): bool
+    {
+        // TODO: if not directly appliable on queryBuilder (not existing field/association on entity)
+        // => return false
+
+        return true;
     }
 }
