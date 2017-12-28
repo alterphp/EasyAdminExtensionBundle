1f56d879b25aac6c8d066f8bc0edc23a09a57bc5
PéCé <pc.bertineau@alterphp.com>
2017-12-29 00:44:40 +0100

    Adds Model for admin user and group

--- src/EasyAdminExtensionBundle.php
+++ src/EasyAdminExtensionBundle.php
@@ -3,6 +3,7 @@
 namespace AlterPHP\EasyAdminExtensionBundle;
 
 use AlterPHP\EasyAdminExtensionBundle\DependencyInjection\Compiler\TwigPathPass;
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -14,5 +15,28 @@ class EasyAdminExtensionBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new TwigPathPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION);
+
+        $this->addRegisterMappingsPass($container);
+    }
+
+    /**
+     * Register storage mapping for model-based persisted objects from EasyAdminExtension.
+     * Much inspired from FOSUserBundle implementation.
+     *
+     * @see  https://github.com/FriendsOfSymfony/FOSUserBundle/blob/master/FOSUserBundle.php
+     *
+     * @param ContainerBuilder $container
+     */
+    private function addRegisterMappingsPass(ContainerBuilder $container)
+    {
+        $easyAdminExtensionBundlePath = dirname((new \ReflectionClass($this))->getFilename());
+
+        $mappings = array(
+            realpath($easyAdminExtensionBundlePath.'/Resources/config/doctrine-mapping') => 'AlterPHP\EasyAdminExtensionBundle\Model',
+        );
+
+        if (class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
+            $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings));
+        }
     }
 }
---
+++ src/Model/AdminUser.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace AlterPHP\EasyAdminExtensionBundle\Model;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Storage agnostic admin user object.
+ */
+class AdminUser implements UserInterface
+{
+    /**
+     * @var mixed
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @var Collection
+     */
+    protected $groups;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->groups = new ArrayCollection();
+    }
+
+    /**
+     * Get id.
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Get groups.
+     *
+     * @return Collection
+     */
+    public function getGroups(): Collection
+    {
+        return $this->groups;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRoles()
+    {
+        $roles = array('ROLE_ADMIN');
+
+        foreach ($this->getGroups() as $group) {
+            $roles = array_merge($roles, $group->getRoles());
+        }
+
+        return array_unique($roles);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPassword()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSalt()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function eraseCredentials()
+    {
+    }
+}
