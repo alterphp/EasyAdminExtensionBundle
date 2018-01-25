6b0a087afaa7f834bf910b6817dcbcb5f4d0c9dd
PéCé <pc.bertineau@alterphp.com>
2018-01-25 22:55:55 +0100

    Use TwigExtension instead of ConfigPass to prune menu items

---
+++ src/Twig/MenuExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AlterPHP\EasyAdminExtensionBundle\Twig;
+
+use Symfony\Component\Form\FormView;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class MenuExtension extends AbstractExtension
+{
+    protected $menuHelper;
+
+    public function __construct($menuHelper)
+    {
+        $this->menuHelper = $menuHelper;
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new TwigFilter('prune_menu_items', array($this, 'pruneMenuItems')),
+        );
+    }
+
+    public function pruneMenuItems(array $menuConfig)
+    {
+        return $this->menuHelper->pruneMenuItems($menuConfig);
+    }
+}
