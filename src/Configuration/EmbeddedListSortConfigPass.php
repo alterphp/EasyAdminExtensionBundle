d39f2d6cf965aa3a7574693cfb7fd9d3f7bff23b
PéCé <pc.bertineau@alterphp.com>
2018-02-01 18:36:57 +0100

    Sort embedddList according to list config

---
+++ src/Configuration/EmbeddedListSortConfigPass.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AlterPHP\EasyAdminExtensionBundle\Configuration;
+
+use EasyCorp\Bundle\EasyAdminBundle\Configuration\ConfigPassInterface;
+
+/**
+ * Initializes the configuration for all the views of each entity, which is
+ * needed when some entity relies on the default configuration for some view.
+ */
+class EmbeddedListSortConfigPass implements ConfigPassInterface
+{
+    public function process(array $backendConfig)
+    {
+        $backendConfig = $this->processSortingConfig($backendConfig);
+
+        return $backendConfig;
+    }
+
+    /**
+     * @param array $backendConfig
+     *
+     * @return array
+     */
+    private function processSortingConfig(array $backendConfig)
+    {
+        foreach ($backendConfig['entities'] as $entityName => $entityConfig) {
+            if (!isset($entityConfig['embeddedList']['sort'])) {
+                $backendConfig['entities'][$entityName]['embeddedList']['sort'] = $entityConfig['list']['sort'];
+            }
+        }
+
+        return $backendConfig;
+    }
+}
