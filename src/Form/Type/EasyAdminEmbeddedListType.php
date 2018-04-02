43802db8847e0cda923e65604bebc7a179c6d076
PéCé <pc.bertineau@alterphp.com>
2018-04-02 23:09:06 +0200

    EmbeddedList guesser

--- src/Form/Type/EasyAdminEmbeddedListType.php
+++ src/Form/Type/EasyAdminEmbeddedListType.php
@@ -2,7 +2,12 @@
 
 namespace AlterPHP\EasyAdminExtensionBundle\Form\Type;
 
+use AlterPHP\EasyAdminExtensionBundle\EventListener\EmbeddedListTypeGuesserSubscriber;
+use Doctrine\ORM\PersistentCollection as OrmPersistentCollection;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -10,6 +15,23 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class EasyAdminEmbeddedListType extends AbstractType
 {
+    private $embeddedListHelper;
+
+    public function __construct($embeddedListHelper)
+    {
+        $this->embeddedListHelper = $embeddedListHelper;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options) {
+
+        });
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -23,7 +45,30 @@ class EasyAdminEmbeddedListType extends AbstractType
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view->vars['entity'] = $options['entity'];
+        $data = $form->getData();
+
+        $embeddedListEntity = $options['entity'];
+        $embeddedListFilters = $options['filters'];
+
+        if ($data instanceof OrmPersistentCollection) {
+
+            $entityFqcn = $data->getTypeClass()->getName();
+
+            // Guess embeddedList entity if not set
+            if (!isset($embeddedListEntity)) {
+                $embeddedListEntity = $this->embeddedListHelper->guessEntityEntry($entityFqcn);
+            }
+
+            // Guess default filter and let it be overriden by defined filters
+            $embeddedListFilters = array_merge(
+                $this->embeddedListHelper->guessDefaultFilter(
+                    $entityFqcn, $form->getConfig()->getName(), $data->getOwner()
+                ),
+                $embeddedListFilters
+            );
+        }
+
+        $view->vars['entity'] = $embeddedListEntity;
 
         $propertyAccessor = PropertyAccess::createPropertyAccessor();
         $filters = array_map(function ($filter) use ($propertyAccessor, $form) {
@@ -32,7 +77,7 @@ class EasyAdminEmbeddedListType extends AbstractType
             }
 
             return $filter;
-        }, $options['filters']);
+        }, $embeddedListFilters);
 
         $view->vars['filters'] = $filters;
 
@@ -49,9 +94,12 @@ class EasyAdminEmbeddedListType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver
+            ->setDefault('entity', null)
             ->setDefault('filters', array())
             ->setDefault('sort', null)
-            ->setRequired('entity')
+            ->setAllowedTypes('entity', ['null', 'string'])
+            ->setAllowedTypes('filters', ['array'])
+            ->setAllowedTypes('sort', ['null', 'string', 'array'])
         ;
     }
 }
