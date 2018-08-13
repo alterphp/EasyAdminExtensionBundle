4cf5f1087c99a46238537a54a5d796fa0194a03c
Pierre LE PAGE <pierre@kiplin.com>
2018-08-13 11:18:52 +0200

    working

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -47,6 +47,50 @@ class AdminController extends BaseAdminController
     }
 
     /**
+     * {@inheritdoc}
+     */
+    protected function searchAction()
+    {
+        $this->dispatch(EasyAdminEvents::PRE_SEARCH);
+
+        $query = trim($this->request->query->get('query'));
+        // if the search query is empty, redirect to the 'list' action
+        if ('' === $query) {
+            $queryParameters = array_replace($this->request->query->all(), array('action' => 'list', 'query' => null));
+            $queryParameters = array_filter($queryParameters);
+
+            return $this->redirect($this->get('router')->generate('easyadmin', $queryParameters));
+        }
+
+        $searchableFields = $this->entity['search']['fields'];
+        $paginator = $this->findBy(
+            $this->entity['class'],
+            $query,
+            $searchableFields,
+            $this->request->query->get('page', 1),
+            $this->entity['list']['max_results'],
+            isset($this->entity['search']['sort']['field']) ? $this->entity['search']['sort']['field'] : $this->request->query->get('sortField'),
+            isset($this->entity['search']['sort']['direction']) ? $this->entity['search']['sort']['direction'] : $this->request->query->get('sortDirection'),
+            $this->entity['search']['dql_filter']
+        );
+        $fields = $this->entity['list']['fields'];
+
+        $this->dispatch(EasyAdminEvents::POST_SEARCH, array(
+            'fields' => $fields,
+            'paginator' => $paginator,
+        ));
+
+        $parameters = array(
+            'paginator' => $paginator,
+            'fields' => $fields,
+            'delete_form_template' => $this->createDeleteForm($this->entity['name'], '__id__')->createView(),
+            'confirm_form_template' => $this->createConfirmForm($this->entity['name'], '__id__')->createView(),
+        );
+
+        return $this->executeDynamicMethod('render<EntityName>Template', array('search', $this->entity['templates']['list'], $parameters));
+    }
+
+    /**
      * Creates a form used to confirm an action before executing it.
      *
      * @param string     $entityName
@@ -58,8 +102,8 @@ class AdminController extends BaseAdminController
     {
         /** @var FormBuilder $formBuilder */
         $formBuilder = $this->get('form.factory')->createNamedBuilder('confirm_form')
-            ->setAction($this->generateUrl('easyadmin', array('action' => 'post', 'entity' => $entityName, 'id' => $entityId)))
-            ->setMethod('POST')
+            // ->setAction($this->generateUrl('easyadmin', array('action' => 'post', 'entity' => $entityName, 'id' => $entityId)))
+            // ->setMethod('POST')
         ;
         $formBuilder->add('submit', LegacyFormHelper::getType('submit'), array('label' => 'confirm_modal.action', 'translation_domain' => 'EasyAdminBundle'));
         // needed to avoid submitting empty confirm forms (see createDeleteForm())
