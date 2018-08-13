161578adce0b430458c3c073ec9e98abebb4420f
Pierre LE PAGE <pierre@kiplin.com>
2018-08-13 12:27:57 +0200

    undo admincontroller changes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,7 +4,6 @@ namespace AlterPHP\EasyAdminExtensionBundle\Controller;
 
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Util\LegacyFormHelper;
 
 class AdminController extends BaseAdminController
 {
@@ -22,94 +21,6 @@ class AdminController extends BaseAdminController
             'fields' => $fields,
             'masterRequest' => $this->get('request_stack')->getMasterRequest(),
         ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function listAction()
-    {
-        $this->dispatch(EasyAdminEvents::PRE_LIST);
-
-        $fields = $this->entity['list']['fields'];
-        $paginator = $this->findAll($this->entity['class'], $this->request->query->get('page', 1), $this->entity['list']['max_results'], $this->request->query->get('sortField'), $this->request->query->get('sortDirection'), $this->entity['list']['dql_filter']);
-
-        $this->dispatch(EasyAdminEvents::POST_LIST, array('paginator' => $paginator));
-
-        $parameters = array(
-            'paginator' => $paginator,
-            'fields' => $fields,
-            'delete_form_template' => $this->createDeleteForm($this->entity['name'], '__id__')->createView(),
-            'confirm_form_template' => $this->createConfirmForm($this->entity['name'], '__id__')->createView(),
-        );
-
-        return $this->executeDynamicMethod('render<EntityName>Template', array('list', $this->entity['templates']['list'], $parameters));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function searchAction()
-    {
-        $this->dispatch(EasyAdminEvents::PRE_SEARCH);
-
-        $query = trim($this->request->query->get('query'));
-        // if the search query is empty, redirect to the 'list' action
-        if ('' === $query) {
-            $queryParameters = array_replace($this->request->query->all(), array('action' => 'list', 'query' => null));
-            $queryParameters = array_filter($queryParameters);
-
-            return $this->redirect($this->get('router')->generate('easyadmin', $queryParameters));
-        }
-
-        $searchableFields = $this->entity['search']['fields'];
-        $paginator = $this->findBy(
-            $this->entity['class'],
-            $query,
-            $searchableFields,
-            $this->request->query->get('page', 1),
-            $this->entity['list']['max_results'],
-            isset($this->entity['search']['sort']['field']) ? $this->entity['search']['sort']['field'] : $this->request->query->get('sortField'),
-            isset($this->entity['search']['sort']['direction']) ? $this->entity['search']['sort']['direction'] : $this->request->query->get('sortDirection'),
-            $this->entity['search']['dql_filter']
-        );
-        $fields = $this->entity['list']['fields'];
-
-        $this->dispatch(EasyAdminEvents::POST_SEARCH, array(
-            'fields' => $fields,
-            'paginator' => $paginator,
-        ));
-
-        $parameters = array(
-            'paginator' => $paginator,
-            'fields' => $fields,
-            'delete_form_template' => $this->createDeleteForm($this->entity['name'], '__id__')->createView(),
-            'confirm_form_template' => $this->createConfirmForm($this->entity['name'], '__id__')->createView(),
-        );
-
-        return $this->executeDynamicMethod('render<EntityName>Template', array('search', $this->entity['templates']['list'], $parameters));
-    }
-
-    /**
-     * Creates a form used to confirm an action before executing it.
-     *
-     * @param string     $entityName
-     * @param int|string $entityId
-     *
-     * @return Form|FormInterface
-     */
-    protected function createConfirmForm($entityName, $entityId)
-    {
-        /** @var FormBuilder $formBuilder */
-        $formBuilder = $this->get('form.factory')->createNamedBuilder('confirm_form')
-            // ->setAction($this->generateUrl('easyadmin', array('action' => 'post', 'entity' => $entityName, 'id' => $entityId)))
-            // ->setMethod('POST')
-        ;
-        $formBuilder->add('submit', LegacyFormHelper::getType('submit'), array('label' => 'confirm_modal.action', 'translation_domain' => 'EasyAdminBundle'));
-        // needed to avoid submitting empty confirm forms (see createDeleteForm())
-        // $formBuilder->add('_easyadmin_confirm_flag', LegacyFormHelper::getType('hidden'), array('data' => '1'));
-
-        return $formBuilder->getForm();
     }
 
     /**
