e2beb4751482f1354346c04afd1888555b9079a8
Tomas Votruba <tomas.vot@gmail.com>
2018-09-03 11:57:35 +0200

    add exclude_fields option for form fields exclude

---
+++ tests/Configuration/ExcludeFieldsConfigPassSource/DummyEntity.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace AlterPHP\EasyAdminExtensionBundle\Tests\Configuration\ExcludeFieldsConfigPassSource;
+
+final class DummyEntity
+{
+    public $name;
+
+    public $exclude;
+}
