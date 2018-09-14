a959bfb8453de87094282ab141c80471dab9d922
Gonzalo Alonso <gonkpo@gmail.com>
2018-09-14 14:04:12 -0300

    mejoras

--- src/Resources/public/js/select2/i18n/en.js
+++ src/Resources/public/js/select2/i18n/en.js
@@ -1,1 +1,3 @@
-function noResultsCreate(url_action, select_id, field_name) {return '<a href="#" class="btn btn-info" onclick="ajaxModalEntityAction(\''+url_action+'\', \''+select_id+'\', \''+field_name+'\');return false;">Add '+field_name+'</a>';}
+function noResultsCreate(url_action, select_id, field_name) {
+  return '<a href="#" class="btn btn-info" onclick="ajaxModalEntityAction(\''+url_action+'\', \''+select_id+'\', \''+field_name+'\');return false;">Add '+field_name+'</a>';
+}
