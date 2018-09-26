613691982b808a71727e436bc07e71d7d4d02782
Gonzalo Alonso <gonkpo@gmail.com>
2018-09-26 09:40:14 -0300

    remove translation js, use translation EasyAdmin

--- src/Resources/public/js/select2/i18n/es.js
+++
@@ -1,3 +0,0 @@
-function noResultsCreate(url_action, select_id, field_name) {
-  return '<a href="#" class="btn btn-info" onclick="ajaxModalEntityAction(\''+url_action+'\', \''+select_id+'\', \''+field_name+'\');return false;">Agregar '+field_name+'</a>';
-}
