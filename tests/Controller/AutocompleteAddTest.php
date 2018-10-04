1e773525177d3afb423ce6006f51e2ad3a0495dd
Gonzalo Alonso <gonkpo@gmail.com>
2018-10-04 09:29:03 -0300

    - tests/Controller/AutocompleteAddTest - fix README

---
+++ tests/Controller/AutocompleteAddTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AlterPHP\EasyAdminExtensionBundle\Tests\Controller;
+
+use AlterPHP\EasyAdminExtensionBundle\Tests\Fixtures\AbstractTestCase;
+
+class AutocompleteAddTest extends AbstractTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->initClient(array('environment' => 'autocomplete_add'));
+    }
+
+    public function testNewEntityAutocompleteModal()
+    {
+        $crawler = $this->requestNewView('Product');
+        $this->assertSame(1, $crawler->filter('select#product_category_autocomplete[data-easyadmin-autocomplete-action-url="/admin/?action=newAjax&entity=Category"]')->count());
+
+        $crawlerAjax = $this->requestNewAjaxView('Category');
+        $form = $crawlerAjax->filter('form[name=category]')->form(
+            ['category[name]' => 'New Ajax Category']
+        );
+        $crawlerAjax = $this->client->submit($form);
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+}
