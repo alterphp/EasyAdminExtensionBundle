1bf260747861592eb983404f0b2bead2d4bd580b
Jean-Pascal Devierne <jean-pascal@novaway.fr>
2018-12-06 16:40:14 +0100

    Added GTE, LT, LTE filter text types

---
+++ src/Form/Type/Operator/LowerThanFilterTextType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AlterPHP\EasyAdminExtensionBundle\Form\Type\Operator;
+
+use AlterPHP\EasyAdminExtensionBundle\Form\Transformer\Operator\LowerThanModelTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LowerThanFilterTextType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new LowerThanModelTransformer());
+    }
+
+
+    public function getParent()
+    {
+        return TextType::class;
+    }
+}
---
+++ src/Form/Type/Operator/LowerThanOrEqualFilterTextType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AlterPHP\EasyAdminExtensionBundle\Form\Type\Operator;
+
+
+use AlterPHP\EasyAdminExtensionBundle\Form\Transformer\Operator\LowerThanOrEqualModelTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LowerThanOrEqualFilterTextType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new LowerThanOrEqualModelTransformer());
+    }
+
+
+    public function getParent()
+    {
+        return TextType::class;
+    }
+}
