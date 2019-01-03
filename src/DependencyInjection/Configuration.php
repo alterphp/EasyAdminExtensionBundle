173ff0f2bf9b4f8b6eb69b8c399ec4d7df725b2c
PéCé <pc.bertineau@alterphp.com>
2019-01-03 17:33:07 +0100

    Revert "Remove Sf4.2 deprecation on TreeBuilder constructor"
    This reverts commit 803a5a2bcfa827db60f2bd0b55f144aa896adfb5.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,10 +17,10 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('easy_admin_extension');
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('easy_admin_extension');
 
-        $treeBuilder
-            ->getRootNode()
+        $rootNode
             ->children()
                 ->arrayNode('custom_form_types')
                     ->useAttributeAsKey('short_name')
