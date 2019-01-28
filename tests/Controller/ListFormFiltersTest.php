2a32db613d5edba45a245b6c4fd413e1da08c2ea
PéCé <pc.bertineau@alterphp.com>
2019-01-28 15:29:44 +0100

    Fix tests

--- tests/Controller/ListFormFiltersTest.php
+++ tests/Controller/ListFormFiltersTest.php
@@ -21,15 +21,15 @@ class ListFormFiltersTest extends AbstractTestCase
 
         $listFormFiltersCrawler = $crawler->filter('#list-form-filters');
 
-        $this->assertSame(1, $listFormFiltersCrawler->filter('select#form_filters_oddEven[multiple]')->count());
-        $this->assertSame(1, $listFormFiltersCrawler->filter('select#form_filters_category_autocomplete[multiple]')->count());
-        $this->assertSame(1, $listFormFiltersCrawler->filter('select#form_filters_replenishmentType[multiple]')->count());
-        $this->assertSame(1, $listFormFiltersCrawler->filter('select#form_filters_enabled')->count());
+        $this->assertSame(1, $listFormFiltersCrawler->filter('select#form_filters_oddEven_value[multiple]')->count());
+        $this->assertSame(1, $listFormFiltersCrawler->filter('select#form_filters_category_value_autocomplete[multiple]')->count());
+        $this->assertSame(1, $listFormFiltersCrawler->filter('select#form_filters_replenishmentType_value[multiple]')->count());
+        $this->assertSame(1, $listFormFiltersCrawler->filter('select#form_filters_enabled_value')->count());
     }
 
     public function testFormSingleFilterIsApplied()
     {
-        $crawler = $this->requestListView('Product', [], ['enabled' => false]);
+        $crawler = $this->requestListView('Product', [], ['enabled' => [ 'value' => false]]);
 
         $this->assertSame(200, $this->client->getResponse()->getStatusCode());
         $this->assertSame(10, $crawler->filter('#main tr[data-id]')->count());
@@ -37,7 +37,7 @@ class ListFormFiltersTest extends AbstractTestCase
 
     public function testFormSingleEasyadminAutocompleteFilterIsApplied()
     {
-        $crawler = $this->requestListView('Product', [], ['category' => ['autocomplete' => [1]]]);
+        $crawler = $this->requestListView('Product', [], ['category' => [ 'value' => ['autocomplete' => [1]]]]);
 
         $this->assertSame(200, $this->client->getResponse()->getStatusCode());
         $this->assertSame(10, $crawler->filter('#main tr[data-id]')->count());
