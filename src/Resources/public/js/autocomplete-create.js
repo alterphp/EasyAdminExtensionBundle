81544d6ea228468525eba6b4dfef00d14244094a
Gonzalo Alonso <gonkpo@gmail.com>
2019-02-12 13:27:34 -0300

    Fix JS #98
    Autocomplete add new option 'create' -> does not work for me #98

--- src/Resources/public/js/autocomplete-create.js
+++ src/Resources/public/js/autocomplete-create.js
@@ -94,7 +94,8 @@ function initCreateEntityAjaxForm(field_name, select_id) {
     });
   });
 }
-
-$(function () {
-  createAutoCompleteCreateFields();
+window.addEventListener('load', function() {
+  $(function () {
+    createAutoCompleteCreateFields();
+  });
 });
