46b876575d05fff3c115d47d8a22e84a642f68a7
PéCé <pc.bertineau@alterphp.com>
2019-04-02 22:50:13 +0200

    Workaround by prepending TwigBundle config

--- src/DependencyInjection/EasyAdminExtensionExtension.php
+++ src/DependencyInjection/EasyAdminExtensionExtension.php
@@ -2,8 +2,10 @@
 
 namespace AlterPHP\EasyAdminExtensionBundle\DependencyInjection;
 
+use EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -12,7 +14,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * @see http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class EasyAdminExtensionExtension extends Extension
+class EasyAdminExtensionExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -31,5 +33,46 @@ class EasyAdminExtensionExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $twigConfigs = $container->getExtensionConfig('twig');
+
+        $paths = [];
+        // keeping user-configured paths
+        foreach ($twigConfigs as $twigConfig) {
+            if (isset($twigConfig['default_path'])) {
+                $userDefinedTwigDefaultPath = $twigConfig['default_path'];
+            }
+            if (isset($twigConfig['paths'])) {
+                $paths += $twigConfig['paths'];
+            }
+        }
+
+        $twigDefaultPathDefaultValue = $container->getParameterBag()->resolveValue('%kernel.project_dir%/templates');
+        $twigDefaultPath = $userDefinedTwigDefaultPath ?? $twigDefaultPathDefaultValue;
+
+        // Waiting this PR or any alternative is implemented by Symfony itself
+        // @see https://github.com/symfony/symfony/pull/30527
+        // Put back user default path (even not user-defined twig.default_path, but TwigBundle default value)
+        $userDefaultPath = $twigDefaultPath.'/bundles/EasyAdminBundle/';
+        if (file_exists($userDefaultPath)) {
+            $paths[$userDefaultPath] = 'EasyAdmin';
+        }
+
+        // EasyAdminExtension overrides EasyAdmin templates
+        $paths[\dirname(__DIR__).'/Resources/views/'] = 'EasyAdmin';
+
+        // Creates BaseEasyAdmin namespace to specifically target to original EasyAdmin templates
+        $nativeEasyAdminBundleRefl = new \ReflectionClass(EasyAdminBundle::class);
+        if ($nativeEasyAdminBundleRefl->isUserDefined()) {
+            $nativeEasyAdminBundlePath = \dirname((string) $nativeEasyAdminBundleRefl->getFileName());
+            $nativeEasyAdminTwigPath = $nativeEasyAdminBundlePath.'/Resources/views';
+            // Defines a namespace from native EasyAdmin templates
+            $paths[$nativeEasyAdminTwigPath] = 'BaseEasyAdmin';
+        }
+
+        $container->prependExtensionConfig('twig', ['paths' => $paths]);
     }
 }
