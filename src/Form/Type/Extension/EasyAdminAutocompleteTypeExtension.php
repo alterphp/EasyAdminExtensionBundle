b56417401805c0d597683ea26e9934cbb2a1fed2
PéCé <pc.bertineau@alterphp.com>
2019-04-16 11:31:05 +0200

    Remove some sf 4.2 and twig 2.x deprecations (#121)
    * Remove some sf 4.2 and twig 2.x deprecations
    
    * Fix deprecation handler

--- src/Form/Type/Extension/EasyAdminAutocompleteTypeExtension.php
+++ src/Form/Type/Extension/EasyAdminAutocompleteTypeExtension.php
@@ -22,7 +22,15 @@ class EasyAdminAutocompleteTypeExtension extends AbstractTypeExtension
 
     public function getExtendedType()
     {
-        return EasyAdminAutocompleteType::class;
+        $extendedTypes = static::getExtendedTypes();
+        $extendedType = reset($extendedTypes);
+
+        return $extendedType;
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [EasyAdminAutocompleteType::class];
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options)
