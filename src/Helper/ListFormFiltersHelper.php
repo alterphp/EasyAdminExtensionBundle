4439ff5f60337ef3d71eebf75a580b20a9e462fd
bnbc <benjamin.beyssac@gmail.com>
2019-06-18 17:05:31 +0200

    Take role for form filters

--- src/Helper/ListFormFiltersHelper.php
+++ src/Helper/ListFormFiltersHelper.php
@@ -36,15 +36,21 @@ class ListFormFiltersHelper
     private $formCsrfEnabled;
 
     /**
+     * @var Security
+     */
+    private $security;
+
+    /**
      * @param FormFactoryInterface $formFactory
      * @param RequestStack         $requestStack
      * @param bool                 $formCsrfEnabled
      */
-    public function __construct(FormFactoryInterface $formFactory, RequestStack $requestStack, $formCsrfEnabled)
+    public function __construct(FormFactoryInterface $formFactory, RequestStack $requestStack, $formCsrfEnabled, Security $security)
     {
         $this->formFactory = $formFactory;
         $this->requestStack = $requestStack;
         $this->formCsrfEnabled = $formCsrfEnabled;
+        $this->security = $security;
     }
 
     public function getListFormFilters(array $formFilters): FormInterface
@@ -59,21 +65,23 @@ class ListFormFiltersHelper
             );
 
             foreach ($formFilters as $name => $config) {
-                $listFilterformOptions = [
-                    'label' => $config['label'] ?? null,
-                    'translation_domain' => $config['translation_domain'] ?? null,
-                    'required' => false,
-                    'input_type' => $config['type'],
-                    'input_type_options' => $config['type_options'] ?? [],
-                ];
-                if (isset($config['operator'])) {
-                    $listFilterformOptions['operator'] = $config['operator'];
-                }
-                if (isset($config['property'])) {
-                    $listFilterformOptions['property'] = $config['property'];
-                }
+                if(!isset($config['role']) or $this->security->isGranted($config['role'])){
+                    $listFilterformOptions = [
+                        'label' => $config['label'] ?? null,
+                        'translation_domain' => $config['translation_domain'] ?? null,
+                        'required' => false,
+                        'input_type' => $config['type'],
+                        'input_type_options' => $config['type_options'] ?? [],
+                    ];
+                    if (isset($config['operator'])) {
+                        $listFilterformOptions['operator'] = $config['operator'];
+                    }
+                    if (isset($config['property'])) {
+                        $listFilterformOptions['property'] = $config['property'];
+                    }
 
-                $formBuilder->add($name, ListFilterType::class, $listFilterformOptions);
+                    $formBuilder->add($name, ListFilterType::class, $listFilterformOptions);
+                }
             }
 
             $this->listFiltersForm = $formBuilder->setMethod('GET')->getForm();
