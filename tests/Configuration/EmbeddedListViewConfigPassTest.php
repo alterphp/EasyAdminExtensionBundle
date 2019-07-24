780ce9341408b2182d07dad3cb14af5964081c69
pécé <pc.bertineau@alterphp.com>
2019-07-24 22:26:12 +0200

    Fix tests

--- tests/Configuration/EmbeddedListViewConfigPassTest.php
+++ tests/Configuration/EmbeddedListViewConfigPassTest.php
@@ -29,17 +29,20 @@ class EmbeddedListViewConfigPassTest extends \PHPUnit_Framework_TestCase
             'entities' => [
                 'NotSetEntity' => [
                     'embeddedList' => [
+                        'template' => '@EasyAdminExtension/default/embedded_list.html.twig',
                         'open_new_tab' => true,
                     ],
                 ],
                 'SetTrueEntity' => [
                     'embeddedList' => [
                         'open_new_tab' => true,
+                        'template' => '@EasyAdminExtension/default/embedded_list.html.twig',
                     ],
                 ],
                 'SetFalseEntity' => [
                     'embeddedList' => [
                         'open_new_tab' => false,
+                        'template' => '@EasyAdminExtension/default/embedded_list.html.twig',
                     ],
                 ],
             ],
