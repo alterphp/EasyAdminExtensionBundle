1968bb86d7019fa8f54a1120e4344a6854810699
Alexandre Anzalone <alexandre@anzacorp.dev>
2019-11-27 14:29:17 +0100

    Fix symfony 4.2/4.3 deprecations

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('easy_admin_extension');
+        $treeBuilder = new TreeBuilder('easy_admin_extension');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('easy_admin_extension');
+        }
 
         $rootNode
             ->children()
