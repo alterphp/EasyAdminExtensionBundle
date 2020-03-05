d80dcb9eb9abec49ce0431f3a3f0846cf921481f
Dylan Kong A Siou <dylan.kong-a-siou@interieur.gouv.fr>
2020-03-05 16:25:38 +0100

    fix: removed useless import

--- src/Controller/EasyAdminController.php
+++ src/Controller/EasyAdminController.php
@@ -5,10 +5,7 @@ namespace AlterPHP\EasyAdminExtensionBundle\Controller;
 use AlterPHP\EasyAdminExtensionBundle\Security\AdminAuthorizationChecker;
 use AlterPHP\EasyAdminExtensionBundle\Controller\AdminExtensionControllerTrait;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController as BaseEasyAdminControler;
-use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
-use League\Uri\Modifiers\RemoveQueryParams;
-use League\Uri\Schemes\Http;
-use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 class EasyAdminController extends BaseEasyAdminControler
 {
