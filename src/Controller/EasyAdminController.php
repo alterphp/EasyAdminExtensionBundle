40c9b1b18329e92d281c25d15f1f0a024ecc3d3a
Dylan Kong A Siou <dylan.kong-a-siou@interieur.gouv.fr>
2020-03-05 16:30:12 +0100

    fix: removing empty line

--- src/Controller/EasyAdminController.php
+++ src/Controller/EasyAdminController.php
@@ -6,7 +6,6 @@ use AlterPHP\EasyAdminExtensionBundle\Security\AdminAuthorizationChecker;
 use AlterPHP\EasyAdminExtensionBundle\Controller\AdminExtensionControllerTrait;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController as BaseEasyAdminControler;
 
-
 class EasyAdminController extends BaseEasyAdminControler
 {
     use AdminExtensionControllerTrait;
