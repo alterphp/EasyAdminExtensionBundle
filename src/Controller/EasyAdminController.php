778cddaef834f028e0f0418d27af69784fd727ae
PéCé <pc.bertineau@alterphp.com>
2020-03-10 19:03:35 +0100

    Remove useless use statement

--- src/Controller/EasyAdminController.php
+++ src/Controller/EasyAdminController.php
@@ -3,7 +3,6 @@
 namespace AlterPHP\EasyAdminExtensionBundle\Controller;
 
 use AlterPHP\EasyAdminExtensionBundle\Security\AdminAuthorizationChecker;
-use AlterPHP\EasyAdminExtensionBundle\Controller\AdminExtensionControllerTrait;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController as BaseEasyAdminControler;
 
 class EasyAdminController extends BaseEasyAdminControler
